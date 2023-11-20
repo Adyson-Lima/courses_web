21c90469c2d9f0ec509451afd4735c4b2e650684
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-20 17:24:25 -0300

    4° commit - tdd botão novo RED

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,7 +1,14 @@
 
 export default function Courses(){
   return(
-    <div>Courses Crud</div>
+    <div className="card border-primary" style={{marginTop: '20px'}}>
+      <div className="card-header bg-primary" style={{color: '#fff'}}>
+        Courses Crud
+      </div>
+      <div className="card-body">
+
+      </div>
+    </div>
   );
 }
 
--- src/pages/Courses/Courses.test.js
+++ src/pages/Courses/Courses.test.js
@@ -1,8 +1,14 @@
 import { render, screen} from "@testing-library/react";
 import Courses from "./Courses";
 
-test("existe card em Courses.js", () => {
+test("Existe card em Courses.js?", () => {
   render(<Courses />);
   const linkElement = screen.getByText(/Courses Crud/i);
   expect(linkElement).toBeInTheDocument();
-})
+});
+
+test("Existe botão novo em Courses.js?", () => {
+  render(<Courses />);
+  const linkElement = screen.getByText(/Novo/i);
+  expect(linkElement).toBeInTheDocument();
+});
